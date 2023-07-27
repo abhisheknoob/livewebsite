d99b362b5e619a4e5f7e6ea49bbcef16452cae98
abhisheknoob <abhishekchoudhary0601@gmail.com>
2023-07-27 19:23:30 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-// App.js
-import './index.css';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Services from './component/Services';
-import Home from './component/Home';
-import About from './component/About';
-
-import Contact from './component/Contact';
-import Navbar from './component/Navbar';
-
+import React, { useState } from 'react';
 
 function App() {
+  const [inputList, setinputList] = useState('');
+  const [item, setitem] = useState([]);
+
+  const additem = (event) => {
+    setinputList(event.target.value);
+  };
+
+  const moveData = () => {
+    setitem((olddata) => {
+      return [...olddata, inputList];
+    });
+    setinputList('');
+  };
+
+  const deleteValue = (index) => {
+    setitem((olddata) => {
+      return olddata.filter((value, i) => i !== index);
+    });
+  };
+
   return (
     <>
-    <Navbar/>
-    <Routes>
-    <Route  path='/Home' element={<Home/>}/>
-    <Route path='/About' element={<About/>}/>
-    <Route path='/Services' element={<Services/>}/>
-    <Route path='/Contact' element={<Contact/>}/>
-    </Routes>
+      <div className='main_div'>
+        <div className='center_div'>
+          <h1>to do list</h1>
+          <br />
+          <div className='todo'>
+            <input
+              type='text'
+              placeholder='Add items'
+              onChange={additem}
+              value={inputList}
+            />
+            <button onClick={moveData}>+</button>
+            <button onClick={()=>{
+              setitem([])
+            }}>delete all</button>
+          </div>
+          <ul>
+            {item.map((val, index) => {
+              return (
+                <li key={index}>
+                  {val}
+                  <button onClick={() => deleteValue(index)}>Delete</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
 
 export default App;
+
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from './App'; // Assuming App is the root component of your application
+import './App.css'
+
+// Wrap the rendering with createRoot instead of ReactDOM.render
+// Note: The second argument is optional and can be used for callback function or options
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement).render(<App />);
 
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
 
 
